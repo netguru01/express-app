48e45f1d8549acdf993ddccebd4bd5396a5b6fa9
Yinka Ash <slankeyinc@gmail.com>
2016-12-15 01:26:41 +0800

    Ready for production

--- routes/contact.js
+++ routes/contact.js
@@ -18,7 +18,7 @@ router.post('/send', function (req, res, next) {
 	});
 	var mailOptions = {
 		from: 'Fastex Logistics <fsxlogic@gmail.com>',
-		to: 'slankeyinc@gmail.com',
+		to: 'fastexlogistic02@gmail.com',
 		subject: 'Contact Us Form Submission',
 		text: 'You got a new contact form filled with the following details... Name: ' + req.body.name + ' Email ' + req.body.email + ' Message ' + req.body.message,
 		html: '<p>You got a new contact form filled with the following details...<p><ul><li>Name ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
