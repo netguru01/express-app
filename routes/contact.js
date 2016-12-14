1c0a2e7856bf79fcf6512356f0931ee0bc84646d
Yinka Ash <slankeyinc@gmail.com>
2016-12-15 00:51:05 +0800

    Ready for production

--- routes/contact.js
+++ routes/contact.js
@@ -9,24 +9,21 @@ router.get('/', function(req, res, next) {
 
 router.post('/send', function (req, res, next) {
 	// body...
-	var transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-	    port: 465,
-	    secure: true, // use SSL, 
-	                  // you can try with TLS, but port is then 587
-	    auth: {
-	      user: 'amostvee@gmail.com', // Your email id
-	      pass: 'Success12#' // Your password
-	    }
+	var smtpTransport = nodemailer.createTransport('SMTP', {
+		service: 'Gmail',
+		auth: {
+			user: 'amostvee@gmail.com',
+			pass: 'Success12#'
+		}
 	});
 	var mailOptions = {
-		from: 'Amos TV <amostvee@gmail.com>',
+		from: 'Amos TVEE <amostvee@gmail.com>',
 		to: 'slankeyinc@gmail.com',
 		subject: 'Contact Us Form Submission',
 		text: 'You got a new contact form filled with the following details... Name: ' + req.body.name + ' Email ' + req.body.email + ' Message ' + req.body.message,
 		html: '<p>You got a new contact form filled with the following details...<p><ul><li>Name ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
 	};
-	transporter.sendMail(mailOptions, function(error, info) {
+	smtpTransport.sendMail(mailOptions, function(error, info) {
 		// body...
 		if (error) {
 			console.log(error);
